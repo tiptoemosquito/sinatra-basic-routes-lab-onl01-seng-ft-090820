05d879228319cda31550737756691082eadaf204
tiptoemosquito <clhoward33@icloud.com>
2020-10-14 09:46:45 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Christina" 
+    end
+
+    get '/hometown' do
+        "My hometown is Edison, New Jersey"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 'Someday We'll All Be Free' by Donny Hathaway"
+    end
 end
